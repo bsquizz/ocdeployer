f76d29756c78ace85d5f18b5a6368cc619161ae4
Brandon Squizzato <bsquizza@redhat.com>
2019-02-13 15:18:54 -0500

    Add jinja2 to setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     keywords=["openshift", "kubernetes"],
     setup_requires=["setuptools_scm"],
     include_package_data=True,
-    install_requires=["sh", "prompter", "pyyaml", "click", "appdirs", "wait_for"],
+    install_requires=["sh", "prompter", "pyyaml", "click", "appdirs", "wait_for", "jinja2"],
     classifiers=[
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
