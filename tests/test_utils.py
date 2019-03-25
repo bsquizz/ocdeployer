1f022edba72bda2e550ef0c41611849d7d56581f
Brandon Squizzato <bsquizza@redhat.com>
2019-03-24 21:49:52 -0400

    Black formatting on tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,6 +3,7 @@ import ocdeployer.utils as utils
 
 import pytest
 
+
 def test_object_merge_merges_lists():
     edit = [1, 3]
     add = [2]
@@ -10,6 +11,7 @@ def test_object_merge_merges_lists():
     utils.object_merge(add, edit)
 
     assert edit == [2, 1, 3]
+
 
 def test_obect_merge_merges_dicts():
     edit = {"thing": "stuff"}
@@ -19,6 +21,7 @@ def test_obect_merge_merges_dicts():
 
     assert edit == {"thing": "stuff", "test": "add"}
 
+
 def test_object_merge_keeps_original_dict_keys():
     edit = {"thing": 3}
     add = {"thing": 1}
@@ -26,6 +29,7 @@ def test_object_merge_keeps_original_dict_keys():
     utils.object_merge(add, edit)
 
     assert edit == {"thing": 3}
+
 
 def test_object_merge_recursively_merges_dicts():
     edit = {"things": {"stuff": 1}}
