5489f15e0590e087b31b94a26d9642cfbfcd5f21
Brandon Squizzato <bsquizza@redhat.com>
2020-01-09 17:08:55 -0500

    Add missing conftest file

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+
+
+def _is_test_path(path):
+    return "envTEST" in path
+
+
+def _patch_exists(path):
+    if _is_test_path(path):
+        print(f"Overriding os.path.exists for path={path}")
+        return True
+
+
+def _patch_isdir(path):
+    if _is_test_path(path):
+        print(f"Overriding os.path.isdir for path={path}")
+        return True
+
+
+def _patch_isfile(path):
+    if _is_test_path(path):
+        print(f"Overriding os.path.isfile for path={path}")
+        return True
+
+
+@pytest.fixture
+def patch_os_path(monkeypatch):
+    monkeypatch.setattr("os.path.exists", _patch_exists)
+    monkeypatch.setattr("os.path.isdir", _patch_isdir)
+    monkeypatch.setattr("os.path.isfile", _patch_isfile)
