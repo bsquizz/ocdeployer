c4bfe5d9de7622784fd8b31bfc931f7ebed474b8
Denis Moiseev <dmoiseev@redhat.com>
2020-02-19 19:42:12 +0100

    Workaround for boolean parameters in env files (#36)
    * workaround boolean parameters in env configs
    
    (cherry picked from commit 092e65fb6d8868f262980d6221518433de1345f4)
    
    * fix formatting

---
+++ tests/test_templates.py
@@ -0,0 +1,19 @@
+import pytest
+
+from ocdeployer.templates import Template
+
+
+@pytest.mark.parametrize(
+    'value,expected',
+    (
+        (True, 'true'),
+        ('True', "'True'"),
+        ('true', "'true'"),
+        ('123', "'123'"),
+        (123, '123'),
+        ('123:123:123', '123:123:123'),
+        ('some text', 'some text')
+    )
+)
+def test_template_oc_param_format(value, expected):
+    assert Template._format_oc_parameter(value) == expected
