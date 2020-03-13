1615ad167817dfe8d68bb81a2217150bf5b7777d
Brandon Squizzato <bsquizza@redhat.com>
2020-03-13 16:18:15 -0400

    Fix image importer tests

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -4,16 +4,16 @@ from ocdeployer.templates import Template
 
 
 @pytest.mark.parametrize(
-    'value,expected',
+    "value,expected",
     (
-        (True, 'true'),
-        ('True', 'True'),
-        ('true', 'true'),
-        ('123', '123'),
-        (123, '123'),
-        ('123:123:123', '123:123:123'),
-        ('some text', 'some text')
-    )
+        (True, "true"),
+        ("True", "True"),
+        ("true", "true"),
+        ("123", "123"),
+        (123, "123"),
+        ("123:123:123", "123:123:123"),
+        ("some text", "some text"),
+    ),
 )
 def test_template_oc_param_format(value, expected):
     assert Template._format_oc_parameter(value) == expected
